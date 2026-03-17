7fa654fc0a12aa0ffd46537983d2a3a5c0e41977
smit6606 <smeetgarala6606@gmail.com>
2026-03-17 16:02:57 +0530

    Configure Git user name and email

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,7 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   images: {
-    domains: ['localhost', 'res.cloudinary.com'],
+    domains: ['localhost', 'res.cloudinary.com', '0bldvnch-5000.inc1.devtunnels.ms', '0bldvnch-3000.inc1.devtunnels.ms'],
   },
   compress: true,
   /* config options here */
